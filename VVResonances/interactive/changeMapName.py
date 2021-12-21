b0fe46a8f7037a4b753ceda827ee83187d4cfdf1
IreneZoi <irene.zoi@cern.ch>
2021-12-22 00:36:37 +0100

    cleaned scripts to rename decorrelation maps

--- VVResonances/interactive/changeMapName.py
+++ VVResonances/interactive/changeMapName.py
@@ -12,9 +12,11 @@ parser.add_option("-c","--cut",dest="cut",help="0p05 or 0p10 etc",default='0p05'
 parser.add_option("-i","--indir",dest="indir",help="input directory",default='')
 parser.add_option("-o","--outdir",dest="outdir",help="output directory",default='')
 parser.add_option("-t","--tagger",dest="tagger",help="W or ZH",default='W')
+parser.add_option("--dec",dest="dec",help="MD or noMD",default='noMD')
+
 (options,args) = parser.parse_args()
 c = options.cut 
-
+print " options.dec ",options.dec
 
 # first rebin pseudodata
 mapfile = "myDeepBoostedMap_"+c+"rho.root"
@@ -22,7 +24,12 @@ print mapfile
 r_file = ROOT.TFile(str(options.indir)+mapfile,"READ")
 print r_file
 mapname = "DeepBoosted_WvsQCD_v_rho_v_pT_scaled_yx"  #DeepBoosted_WvsQCD_v_rho_v_pT_yx"
-if options.tagger=="ZH": mapname = "DeepBoosted_ZHbbvsQCD_v_rho_v_pT_scaled_yx" #"DeepBoosted_ZHbbvsQCD_v_rho_v_pT_yx"
+if options.dec == "MD": mapname = "DeepBoosted_WvsQCD_MD_v_rho_v_pT_scaled_yx"
+if options.tagger=="ZH":
+    print "ZH"
+    mapname = "DeepBoosted_ZHbbvsQCD_v_rho_v_pT_scaled_yx" #"DeepBoosted_ZHbbvsQCD_v_rho_v_pT_yx"
+    if options.dec == "MD": mapname ="DeepBoosted_ZHbbvsQCD_MD_v_rho_v_pT_scaled_yx"
+print " mapname ",mapname
 cutmap = r_file.Get(mapname)
 print cutmap
 #print data.GetXaxis().GetNbins()
--- VVResonances/interactive/changeMapName.sh
+++ VVResonances/interactive/changeMapName.sh
@@ -1,19 +1,17 @@
 #!bin/bash
 
-#indirW=MapsDeltaEta/
-#outdirW=testMapW_fineBinning_Pt_2016-2017-2018/
-indirZH=MapsDeltaEta/DDTMap_ZHbbvsQCD_scaled_2018_withDeltaEta_finerbinning/
-outdirZH=MapsDeltaEta/DDTMap_ZHbbvsQCD_scaled_2018_withDeltaEta_finerbinning/
+year=Run2
+indirW=MapsDeltaEta/DDTMap_WvsQCD_MD_scaled_${year}_withDeltaEta_finerbinning/
+outdirW=MapsDeltaEta/DDTMap_WvsQCD_MD_scaled_${year}_withDeltaEta_finerbinning/
+indirZH=MapsDeltaEta/DDTMap_ZHbbvsQCD_MD_scaled_${year}_withDeltaEta_finerbinning/
+outdirZH=MapsDeltaEta/DDTMap_ZHbbvsQCD_MD_scaled_${year}_withDeltaEta_finerbinning/
 cuts=("0p02" "0p03" "0p05" "0p10" "0p15" "0p20" "0p30" "0p50")
 
 
 for cat in ${cuts[*]}; do
     echo $cat
-#    python changeMapName.py -c $cat -i ${indirW} -o ${outdirW}
-    python changeMapName.py -c $cat -i ${indirZH} -o ${outdirZH} -t "ZH"
-
-
-
+    python changeMapName.py -c $cat -i ${indirZH} -o ${outdirZH} -t "ZH" --dec "MD"
+    python changeMapName.py -c $cat -i ${indirW} -o ${outdirW} -t "W" --dec "MD"
 done
 
 
